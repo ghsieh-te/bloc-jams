5fd8c6a5a6376eb1cd6ff2856b1254dff956a667
Grace Hsieh <ghsieh.te@gmail.com>
2018-04-23 15:51:19 +0800

    Assignment 23 completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,5 +1,12 @@
 var pointsArray = document.getElementsByClassName('point');
 
+var revealPoint = function(point) {
+  point.style.opacity = 1;
+  point.style.transform = "scaleX(1) translateY(0)";
+  point.style.msTransform = "scaleX(1) translateY(0)";
+  point.style.WebkitTransform = "scaleX(1) translateY(0)";
+};
+
 var animatePoints = function(points) {
 
     /*var revealFirstPoint = function() {
@@ -26,8 +33,9 @@ var animatePoints = function(points) {
     revealFirstPoint();
     revealSecondPoint();
     revealThirdPoint();*/
+    forEach(points, revealPoint);
 
-    var revealPoint = function(i) {
+    /*var revealPoint = function(i) {
       points[i].style.opacity = 1;
       points[i].style.transform = "scaleX(1) translateY(0)";
       points[i].style.msTransform = "scaleX(1) translateY(0)";
@@ -36,7 +44,7 @@ var animatePoints = function(points) {
 
     for (var i = 0; i < points.length; i++) {
       revealPoint(i);
-    };
+    };*/
 
 };
 
