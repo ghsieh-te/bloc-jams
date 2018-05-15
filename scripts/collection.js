09e96ab9f14b1d9225d85e8fe5da718e53c166cc
Grace Hsieh <ghsieh.te@gmail.com>
2018-05-15 01:01:54 -0700

    Checkpoint 30 jQuery Collection and Album View Completed

--- scripts/collection.js
+++ scripts/collection.js
@@ -1,4 +1,6 @@
-var collectionItemTemplate =
+/*var collectionItemTemplate =*/
+var buildCollectionItemTemplate = function() {
+     var template =
     '<div class="collection-album-container column fourth">'
   + '  <img src="assets/images/album_covers/01.png"/>'
   + '  <div class="collection-album-info caption">'
@@ -13,14 +15,24 @@ var collectionItemTemplate =
   + '  </div>'
   + '</div>'
   ;
-  window.onload = function() {
 
-    var collectionContainer = document.getElementsByClassName('album-covers')[0];
+  return $(template);
+};
 
-    collectionContainer.innerHTML = '';
+  /*window.onload = function() {*/
+  $(window).load(function() {
+
+    /*var collectionContainer = document.getElementsByClassName('album-covers')[0];
+      collectionContainer.innerHTML = '';*/
+
+    var $collectionContainer = $('.album-covers');
+    $collectionContainer.empty();
 
 
     for (var i = 0; i < 12; i++) {
-        collectionContainer.innerHTML += collectionItemTemplate;
+        /*collectionContainer.innerHTML += collectionItemTemplate;*/
+        var $newThumbnail = buildCollectionItemTemplate();
+        $collectionContainer.append($newThumbnail);
     }
-}
+/*}*/
+});
