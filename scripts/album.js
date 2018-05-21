6f36f859fc4a1ce17b0a3e87ce74f1a07474c151
Grace Hsieh <ghsieh.te@gmail.com>
2018-05-21 00:10:04 -0700

    Checkpoint 32 jQuery Next and Previous Buttons Completed

--- scripts/album.js
+++ scripts/album.js
@@ -121,21 +121,17 @@ var nextSong = function() {
 
  var previousSong = function() {
     var currentSongIndex = trackIndex(currentAlbum, currentSongFromAlbum);
-    // Note that we're _decrementing_ the index here
     currentSongIndex--;
 
     if (currentSongIndex < 0) {
         currentSongIndex = currentAlbum.songs.length - 1;
     }
 
-    // Save the last song number before changing it
     var lastSongNumber = currentlyPlayingSongNumber;
 
-    // Set a new current song
     currentlyPlayingSongNumber = currentSongIndex + 1;
     currentSongFromAlbum = currentAlbum.songs[currentSongIndex];
 
-    // Update the Player Bar information
     updatePlayerBarSong();
 
     $('.main-controls .play-pause').html(playerBarPauseButton);
